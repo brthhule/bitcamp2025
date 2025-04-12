7862b674b7a22ef3b2f440b50e32a10234f0ac18
brthhule <brennenle@yahoo.com>
2025-04-12 00:55:54 -0400

    Linked front end + back end

--- front-end/src/model.jsx
+++ front-end/src/model.jsx
@@ -1,13 +1,11 @@
 import axios from 'axios'
 
 
-export function getDate () {
-    axios.get("http://localhost:3000/date", {})
-    .then(res => {
-        console.log(res.data)
-    })
-    .catch(error => {
-        console.log(error)
-    });
-}
-
+export function getDate() {
+    return axios.get("http://localhost:3000/date")
+      .then(res => res.data)
+      .catch(error => {
+        console.error("Error fetching date:", error);
+        throw error;
+      });
+  }
--- front-end/src/screens/JournalEntryScreen.jsx
+++ front-end/src/screens/JournalEntryScreen.jsx
@@ -1,9 +1,11 @@
 import { useState } from "react"
 import "../styles/journalentry.css"
 import { getDate } from "../model";
+import { useEffect } from "react";
 
 export default function JournalEntryScreen() {
     const [inputValue, setInputValue] = useState('');
+    const [date, setDate] = useState('');
 
     const handleChange = (e) => {
         setInputValue(e.target.value);
@@ -13,9 +15,15 @@ export default function JournalEntryScreen() {
         console.log(inputValue)
     }
 
+    useEffect(() => {
+        getDate().then(date => {
+          setDate(date); // for example
+        });
+      }, []);
+
     return (
         <div className="container">
-            <div className="date">{getDate()}</div>
+            <div className="date">{date}</div>
             
             <div className = "text-input">
                 <input type="text" value={inputValue} onChange={handleChange} />
