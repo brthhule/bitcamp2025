c82b2bc494144391449d7079c18d528f77563c97
brthhule <brennenle@yahoo.com>
2025-04-12 00:00:10 -0400

    router works (pre merge version)

--- front-end/src/screens/HomeScreen.jsx
+++ front-end/src/screens/HomeScreen.jsx
@@ -1,11 +1,11 @@
 import { Navigate } from "react-router-dom"
 
+
 export default function HomeScreen() {
+
     return(
         <>
-            <div onClick={<Navigate to="/journal"/>}>
-                Hello!
-            </div>
+            hello
             
         </>
     )
